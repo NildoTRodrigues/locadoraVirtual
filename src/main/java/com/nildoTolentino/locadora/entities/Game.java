6aa76c7e55fe051b13f1b09f1989baa7c89d6338
Nildo Tolentino <nildotrodrigues@gmail.com>
2023-11-15 14:26:52 -0300

    Game Seed - populado tabela com import.sql

--- src/main/java/com/nildoTolentino/locadora/entities/Game.java
+++ src/main/java/com/nildoTolentino/locadora/entities/Game.java
@@ -2,6 +2,7 @@ package com.nildoTolentino.locadora.entities;
 
 import java.util.Objects;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -20,7 +21,9 @@ public class Game {
 	private String plataforma;
 	private Double score;
 	private String imgUrl;
+	@Column(columnDefinition = "TEXT")
 	private String descricaoCurta;
+	@Column(columnDefinition = "TEXT")
 	private String descricaoLonga;
 	
 	public Game() {}
