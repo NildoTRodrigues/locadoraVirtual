f2d5a2ac254f07901e959ad84e20c5a1de8b4bb0
Nildo Tolentino <nildotrodrigues@gmail.com>
2023-11-19 11:21:55 -0300

    Get Game by id  (consulta por id )

--- src/main/java/com/nildoTolentino/locadora/Controllers/GameController.java
+++ src/main/java/com/nildoTolentino/locadora/Controllers/GameController.java
@@ -4,10 +4,12 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.nildoTolentino.locadora.Services.GameService;
+import com.nildoTolentino.locadora.dto.GameDTO;
 import com.nildoTolentino.locadora.dto.GameMinDTO;
 
 @RestController
@@ -16,6 +18,16 @@ public class GameController {
 
 @Autowired	
 private GameService gameServiceInjetado;
+
+@GetMapping(value = "/{id}")
+   public GameDTO listTbGameNoControlerID(@PathVariable Long id){
+	   GameDTO result = gameServiceInjetado.listaPorID(id);
+	   return result;
+}
+
+
+
+
 @GetMapping
    List<GameMinDTO> listTbGameNoControler(){
 	   List<GameMinDTO> result = gameServiceInjetado.listaRegsGame();
--- src/main/java/com/nildoTolentino/locadora/Services/GameService.java
+++ src/main/java/com/nildoTolentino/locadora/Services/GameService.java
@@ -4,8 +4,11 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.nildoTolentino.locadora.dto.GameDTO;
 import com.nildoTolentino.locadora.dto.GameMinDTO;
+import com.nildoTolentino.locadora.entities.Game;
 import com.nildoTolentino.locadora.repository.GameRepository;
 
 @Service
@@ -14,6 +17,15 @@ public class GameService {
 	@Autowired
 	private GameRepository injetaTbGames;
 
+	@Transactional(readOnly = true)
+	public GameDTO listaPorID(Long gameId) {
+		Game result = injetaTbGames.findById(gameId).get();
+		GameDTO dto = new GameDTO(result) ;
+		return dto;
+				
+	}
+	
+	@Transactional(readOnly = true)
 	public List<GameMinDTO> listaRegsGame() {
 		var result = injetaTbGames.findAll();
 		List<GameMinDTO> dto = result.stream().map(x -> new GameMinDTO(x)).toList();
